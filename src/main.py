94e5e64ed748cb4d80198ae861cf9e6251fe9063
TomLebeda <tom.lebeda@gmail.com>
2023-05-18 12:33:28 +0200

    improved visualization of blob explorations

--- src/main.py
+++ src/main.py
@@ -17,28 +17,3 @@ explore_blobs(list(range(50)), imgs_annotated, IMAGES_PATH)
 # explore_hsv_channels(list(range(100)), imgs_annotated, IMAGES_PATH)
 # explore_thresholding(GOOD_IMAGES_ID, imgs_annotated, IMAGES_PATH, 10)
 
-# for img_ID in list(range(100)):
-# img_ID = 14
-# img_fname = imgs_annotated[img_ID]['@name']
-# img = skimage.io.imread(IMAGES_PATH + img_fname)
-#
-# plt.subplot(3, 1, 1)
-# plt.imshow(img)
-# plt.title('original')
-#
-# img = skimage.color.rgb2hsv(img)
-#
-# hue = img[:, :, 0]   # the bloby one
-# sat = img[:, :, 1]   # the sharp one
-#
-# plt.subplot(3, 1, 2)
-# plt.imshow(hue)
-# plt.title('hue')
-#
-# blob = extract_blob_area(hue)
-#
-# plt.subplot(3, 1, 3)
-# plt.imshow(blob)
-# plt.title('blob')
-#
-# plt.show()
--- src/utils.py
+++ src/utils.py
@@ -6,11 +6,14 @@ import numpy as np
 import skimage
 from typing import List
 
-def explore_rgb_channels(ids: list[int], imgs_annotated: list[dict], imgs_path: str) -> None:
-    """ 
+
+def explore_rgb_channels(
+    ids: list[int], imgs_annotated: list[dict], imgs_path: str
+) -> None:
+    """
     Explore RGB channels of images.
 
-    Each image is shown separately (one at a time) in popup window and after closing it the next one is shown. 
+    Each image is shown separately (one at a time) in popup window and after closing it the next one is shown.
 
 
     Parameters:
@@ -24,25 +27,28 @@ def explore_rgb_channels(ids: list[int], imgs_annotated: list[dict], imgs_path: 
     imgs_path: str
         path to the folder where the actual images are stored
     """
-    channels = ["Red", "Green", "Blue"]
+    channels = ['Red', 'Green', 'Blue']
     for i in ids:
-        img_ID = i 
-        img_fname = imgs_annotated[img_ID]["@name"]
+        img_ID = i
+        img_fname = imgs_annotated[img_ID]['@name']
         img = skimage.io.imread(imgs_path + img_fname)
 
         fig = plt.figure()
         for c in range(3):
-            plt.subplot(3, 1, c+1)
+            plt.subplot(3, 1, c + 1)
             plt.imshow(img[:, :, c], cmap='gray')
-            plt.title(channels[c]+" channel")
-        fig.suptitle(f"Image: {img_fname} \n (ID {img_ID})")
+            plt.title(channels[c] + ' channel')
+        fig.suptitle(f'Image: {img_fname} \n (ID {img_ID})')
         plt.show()
 
-def explore_blobs(ids: list[int], imgs_annotated: list[dict], imgs_path: str) -> None:
-    """ 
+
+def explore_blobs(
+    ids: list[int], imgs_annotated: list[dict], imgs_path: str
+) -> None:
+    """
     Explore blobs detected in images.
 
-    Each image is shown separately (one at a time) in popup window and after closing it the next one is shown. 
+    Each image is shown separately (one at a time) in popup window and after closing it the next one is shown.
 
 
     Parameters:
@@ -56,10 +62,9 @@ def explore_blobs(ids: list[int], imgs_annotated: list[dict], imgs_path: str) ->
     imgs_path: str
         path to the folder where the actual images are stored
     """
-    channels = ["Red", "Green", "Blue"]
     for i in ids:
-        img_ID = i 
-        img_fname = imgs_annotated[img_ID]["@name"]
+        img_ID = i
+        img_fname = imgs_annotated[img_ID]['@name']
         img = skimage.io.imread(imgs_path + img_fname)
 
         fig = plt.figure()
@@ -68,9 +73,7 @@ def explore_blobs(ids: list[int], imgs_annotated: list[dict], imgs_path: str) ->
         plt.imshow(img)
         plt.title('original')
 
-        img = skimage.color.rgb2hsv(img)
-
-        hue = img[:, :, 0]   # the bloby one
+        hue = skimage.color.rgb2hsv(img)[:, :, 0]   # the bloby one
 
         plt.subplot(3, 1, 2)
         plt.imshow(hue)
@@ -79,17 +82,22 @@ def explore_blobs(ids: list[int], imgs_annotated: list[dict], imgs_path: str) ->
         blob = extract_blob_area(hue)
 
         plt.subplot(3, 1, 3)
-        plt.imshow(blob)
+        # plt.imshow(blob)
+        plt.imshow(img, cmap='gray')
+        plt.imshow(blob, alpha=0.4 * blob)
         plt.title('blob')
 
-        fig.suptitle(f"Image: {img_fname} \n (ID {img_ID})")
+        fig.suptitle(f'Image: {img_fname} \n (ID {img_ID})')
         plt.show()
 
-def explore_hsv_channels(ids: List[int], imgs_annotated: list[dict], imgs_path: str) -> None:
-    """ 
+
+def explore_hsv_channels(
+    ids: List[int], imgs_annotated: list[dict], imgs_path: str
+) -> None:
+    """
     Explore HSV channels of images.
 
-    Each image is shown separately (one at a time) in popup window and after closing it the next one is shown. 
+    Each image is shown separately (one at a time) in popup window and after closing it the next one is shown.
 
 
     Parameters:
@@ -103,45 +111,47 @@ def explore_hsv_channels(ids: List[int], imgs_annotated: list[dict], imgs_path: 
     imgs_path: str
         path to the folder where the actual images are stored
     """
-    channels = ["Hue", "Saturation", "Value"]
+    channels = ['Hue', 'Saturation', 'Value']
     for i in ids:
-        img_ID = i 
-        img_fname = imgs_annotated[img_ID]["@name"]
+        img_ID = i
+        img_fname = imgs_annotated[img_ID]['@name']
         img = skimage.io.imread(imgs_path + img_fname)
         gray = skimage.color.rgb2gray(img)
 
         fig = plt.figure()
         plt.subplot(3, 2, 1)
         plt.imshow(img)
-        plt.title("original")
+        plt.title('original')
 
         img = skimage.color.rgb2hsv(img)
         plt.subplot(3, 2, 3)
         plt.imshow(img[:, :, 0])
-        plt.title("hue")
+        plt.title('hue')
 
         plt.subplot(3, 2, 4)
-        plt.imshow(1-img[:, :, 1])
-        plt.title("saturation")
+        plt.imshow(1 - img[:, :, 1])
+        plt.title('saturation')
 
         plt.subplot(3, 2, 5)
         plt.imshow(img[:, :, 2])
-        plt.title("value")
+        plt.title('value')
 
         plt.subplot(3, 2, 6)
         plt.imshow(gray)
-        plt.title("gray")
+        plt.title('gray')
 
-        fig.suptitle(f"Image: {img_fname} \n (ID {img_ID})")
+        fig.suptitle(f'Image: {img_fname} \n (ID {img_ID})')
         plt.show()
 
 
-def explore_thresholding(ids: list[int], imgs_annotated: list[dict], imgs_path: str, percentage: int) -> None:
-    """ 
+def explore_thresholding(
+    ids: list[int], imgs_annotated: list[dict], imgs_path: str, percentage: int
+) -> None:
+    """
     Explore results of thresholding.
 
-    Each image is shown separately (one at a time) in popup window and after closing it the next one is shown. 
-    The thresholded map is overlayed on top of the image with color according to used color channel. 
+    Each image is shown separately (one at a time) in popup window and after closing it the next one is shown.
+    The thresholded map is overlayed on top of the image with color according to used color channel.
     The threshold value is computed as bottom  percentile of the whole image (based on the given percentage).
 
 
@@ -158,28 +168,29 @@ def explore_thresholding(ids: list[int], imgs_annotated: list[dict], imgs_path: 
 
     percentage: int
         Bottom percentile that will be used to compute threshold value.
-        If the value is 10, the thresholding will catch the 10 bottom percent of values. 
+        If the value is 10, the thresholding will catch the 10 bottom percent of values.
     """
     for img_ID in ids:
-        img_fname = imgs_annotated[img_ID]["@name"]
+        img_fname = imgs_annotated[img_ID]['@name']
         img = skimage.io.imread(imgs_path + img_fname)
         fig = plt.figure()
-        fig.suptitle(f"Image: {img_fname} \n (ID {img_ID})")
-        channels = ["Red", "Green", "Blue"]
+        fig.suptitle(f'Image: {img_fname} \n (ID {img_ID})')
+        channels = ['Red', 'Green', 'Blue']
         for c in range(3):
             imgc = img[:, :, c]
             threshold = np.percentile(imgc, percentage)
             mask = 1.0 * (imgc < threshold)
-            plt.subplot(3, 1, c+1)
+            plt.subplot(3, 1, c + 1)
             plt.imshow(imgc, cmap='gray')
-            plt.imshow(mask, alpha = mask, cmap=channels[c]+"s")
+            plt.imshow(mask, alpha=mask, cmap=channels[c] + 's')
         plt.show()
+
 
 def remove_border_areas(img: np.ndarray) -> np.ndarray:
     """
     Removes areas that are touching the border of the image.
     Returns a copy of the input image without the border-touching areas.
-    
+
     Parameters:
     -----------
     img: np.ndarray
@@ -189,20 +200,23 @@ def remove_border_areas(img: np.ndarray) -> np.ndarray:
     img[:, -1] = 1.0
     img[0, :] = 1.0
     img[-1, :] = 1.0
-    img = skimage.segmentation.flood_fill(img, (0, 0), 0, footprint=[[0, 1, 0], [1, 1, 1], [0, 1, 0]])
+    img = skimage.segmentation.flood_fill(
+        img, (0, 0), 0, footprint=[[0, 1, 0], [1, 1, 1], [0, 1, 0]]
+    )
     return img
+
 
 def extract_blob_area(hue_channel: np.ndarray) -> np.ndarray:
     """
-    Extracts a blob-like area from hue channel of image in HSV colorspace that should indicate where the scar is. 
-    
+    Extracts a blob-like area from hue channel of image in HSV colorspace that should indicate where the scar is.
+
     Parameters:
     -----------
     hue_channel: np.ndarray
         Hue channel of HSV colorspace image (2D array) that will have its border-touching areas deleted
     """
-    # thresholding 
-    hue = 1 * (hue_channel > 0.5) 
+    # thresholding
+    hue = 1 * (hue_channel > 0.5)
 
     # label the areas
     l1 = skimage.morphology.label(hue, connectivity=1)
@@ -217,6 +231,8 @@ def extract_blob_area(hue_channel: np.ndarray) -> np.ndarray:
         l3 = remove_border_areas(1 * (l3 > 0))
 
     # do some morphology magic to smooth and close the remaining areas
-    kernel_size = int(min(l3.shape) / 20) # 20 was found experimentally as a good value for most images
+    kernel_size = int(
+        min(l3.shape) / 20
+    )   # 20 was found experimentally as a good value for most images
     kernel = skimage.morphology.disk(kernel_size)
     return skimage.morphology.binary_closing(l3, kernel)
