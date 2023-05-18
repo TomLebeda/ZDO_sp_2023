de0fa078ccbfa405c18c4bca709d9e2a1d5b050a
TomLebeda <tom.lebeda@gmail.com>
2023-05-18 12:14:50 +0200

    finished blob extraction and exploration from hue

--- src/main.py
+++ src/main.py
@@ -1,11 +1,7 @@
 import xmltodict
-from pprint import pprint
-import json
 from matplotlib import pyplot as plt
-import numpy as np
 from utils import *
-import skimage.io
-import typing
+import skimage
 
 ANNOTATION_PATH = './data/annotations.xml'
 IMAGES_PATH = './data/images/default/'
@@ -17,44 +13,32 @@ with open(ANNOTATION_PATH) as f:
 imgs_annotated = doc['annotations']['image']
 
 # explore_rgb_channels(list(range(50)), imgs_annotated, IMAGES_PATH)
+explore_blobs(list(range(50)), imgs_annotated, IMAGES_PATH)
 # explore_hsv_channels(list(range(100)), imgs_annotated, IMAGES_PATH)
 # explore_thresholding(GOOD_IMAGES_ID, imgs_annotated, IMAGES_PATH, 10)
 
-
-for img_ID in list(range(100)):
-    img_fname = imgs_annotated[img_ID]['@name']
-    img = skimage.io.imread(IMAGES_PATH + img_fname)
-
-    plt.subplot(4, 1, 1)
-    plt.imshow(img)
-    plt.title('original')
-
-    img = skimage.color.rgb2hsv(img)
-
-    hue = img[:, :, 0]   # the bloby one
-    sat = img[:, :, 1]   # the sharp one
-
-    plt.subplot(4, 1, 2)
-    plt.imshow(hue)
-    plt.title('hue')
-
-    hue = 1.0 * (hue > 0.5)
-
-    plt.subplot(4, 1, 3)
-    plt.imshow(hue)
-    plt.title('thresholded')
-
-    labeled = skimage.morphology.label(hue)
-    # print(labeled.size)
-    # if labeled.size > 1:
-    hue = remove_border_areas(hue)
-    kernel_size = int(min(hue.shape) / 20) # 20 was found experimentally as a good value for most images
-    hue = skimage.morphology.binary_closing(hue, skimage.morphology.disk(kernel_size))
-    hue = skimage.morphology.remove_small_objects(hue, 70) # 50 was found experimentally as a good value for most images
-    # hue = skimage.morphology.remove_small_holes(hue, 64) # 50 was found experimentally as a good value for most images
-
-    plt.subplot(4, 1, 4)
-    plt.imshow(hue)
-    plt.title('edited')
-
-    plt.show()
+# for img_ID in list(range(100)):
+# img_ID = 14
+# img_fname = imgs_annotated[img_ID]['@name']
+# img = skimage.io.imread(IMAGES_PATH + img_fname)
+#
+# plt.subplot(3, 1, 1)
+# plt.imshow(img)
+# plt.title('original')
+#
+# img = skimage.color.rgb2hsv(img)
+#
+# hue = img[:, :, 0]   # the bloby one
+# sat = img[:, :, 1]   # the sharp one
+#
+# plt.subplot(3, 1, 2)
+# plt.imshow(hue)
+# plt.title('hue')
+#
+# blob = extract_blob_area(hue)
+#
+# plt.subplot(3, 1, 3)
+# plt.imshow(blob)
+# plt.title('blob')
+#
+# plt.show()
--- src/utils.py
+++ src/utils.py
@@ -38,6 +38,53 @@ def explore_rgb_channels(ids: list[int], imgs_annotated: list[dict], imgs_path: 
         fig.suptitle(f"Image: {img_fname} \n (ID {img_ID})")
         plt.show()
 
+def explore_blobs(ids: list[int], imgs_annotated: list[dict], imgs_path: str) -> None:
+    """ 
+    Explore blobs detected in images.
+
+    Each image is shown separately (one at a time) in popup window and after closing it the next one is shown. 
+
+
+    Parameters:
+    -----------
+    ids: list[int]
+        list of IDs of images that will be shown
+
+    imgs_annotated: list[dict]
+        list of dictionaries that represent the images (from annotated data)
+
+    imgs_path: str
+        path to the folder where the actual images are stored
+    """
+    channels = ["Red", "Green", "Blue"]
+    for i in ids:
+        img_ID = i 
+        img_fname = imgs_annotated[img_ID]["@name"]
+        img = skimage.io.imread(imgs_path + img_fname)
+
+        fig = plt.figure()
+
+        plt.subplot(3, 1, 1)
+        plt.imshow(img)
+        plt.title('original')
+
+        img = skimage.color.rgb2hsv(img)
+
+        hue = img[:, :, 0]   # the bloby one
+
+        plt.subplot(3, 1, 2)
+        plt.imshow(hue)
+        plt.title('hue')
+
+        blob = extract_blob_area(hue)
+
+        plt.subplot(3, 1, 3)
+        plt.imshow(blob)
+        plt.title('blob')
+
+        fig.suptitle(f"Image: {img_fname} \n (ID {img_ID})")
+        plt.show()
+
 def explore_hsv_channels(ids: List[int], imgs_annotated: list[dict], imgs_path: str) -> None:
     """ 
     Explore HSV channels of images.
@@ -129,9 +176,47 @@ def explore_thresholding(ids: list[int], imgs_annotated: list[dict], imgs_path: 
         plt.show()
 
 def remove_border_areas(img: np.ndarray) -> np.ndarray:
+    """
+    Removes areas that are touching the border of the image.
+    Returns a copy of the input image without the border-touching areas.
+    
+    Parameters:
+    -----------
+    img: np.ndarray
+        binary image that will have its border-touching areas deleted
+    """
     img[:, 0] = 1.0
     img[:, -1] = 1.0
     img[0, :] = 1.0
     img[-1, :] = 1.0
     img = skimage.segmentation.flood_fill(img, (0, 0), 0, footprint=[[0, 1, 0], [1, 1, 1], [0, 1, 0]])
     return img
+
+def extract_blob_area(hue_channel: np.ndarray) -> np.ndarray:
+    """
+    Extracts a blob-like area from hue channel of image in HSV colorspace that should indicate where the scar is. 
+    
+    Parameters:
+    -----------
+    hue_channel: np.ndarray
+        Hue channel of HSV colorspace image (2D array) that will have its border-touching areas deleted
+    """
+    # thresholding 
+    hue = 1 * (hue_channel > 0.5) 
+
+    # label the areas
+    l1 = skimage.morphology.label(hue, connectivity=1)
+
+    # remove small objects and re-label
+    removed = skimage.morphology.remove_small_objects(l1, 70)
+    l2 = skimage.morphology.label(removed, connectivity=1)
+
+    # remove border areas
+    l3, c3 = skimage.morphology.label(l2, return_num=True, connectivity=1)
+    if c3 > 1:
+        l3 = remove_border_areas(1 * (l3 > 0))
+
+    # do some morphology magic to smooth and close the remaining areas
+    kernel_size = int(min(l3.shape) / 20) # 20 was found experimentally as a good value for most images
+    kernel = skimage.morphology.disk(kernel_size)
+    return skimage.morphology.binary_closing(l3, kernel)
